3101f54e14da205d965a21ae5d5dee07c16b7ad8
nicoschoenteich <nicolai.schoenteich@sap.com>
2025-01-30 14:49:40 +0100

    redo original structure, add dev:mock script

--- bookshop/webapp/model/formatter.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([], function () {
-	"use strict"
-	return {
-		inputLowerThanStock: function (availableStock) {
-            const inputValue = this.getView().byId("stepInput").getValue()
-            return inputValue <= availableStock
-		}
-	}
-})
